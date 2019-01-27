f480404751bd1b5caeb2099206991d86e03e2ee3
Nathan <nathanamar19@gmail.com>
2019-01-27 23:43:33 +0100

    Create Zombie.cs
    Add Zombie class

---
+++ Zombie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOI_GRA_AMAR_AMAR
+{
+    class Zombie : Monstre
+    {
+        //Attribut
+        private int degreDecomposition;
+        private string teint;
+
+        //Constructeur
+        public Zombie(string fonction, int matricule, string nom, string prenom, string type, string fonctionFac, int cagnotte, string affectation, string teint, int degreDecomposition) : base(fonction, matricule, nom, prenom, type, fonctionFac, cagnotte, affectation)
+        {
+            this.degreDecomposition = degreDecomposition;
+            this.teint = teint;
+        }
+        //ToString
+        public override string ToString()
+        {
+            return base.ToString() + "\t;;Teint: " + this.teint +
+                "\t;;Degre decomposition: " + this.degreDecomposition+"\n";
+
+        }
+    }
+
+}
